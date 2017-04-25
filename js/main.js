9eb5e4f33e05cb97f64e6993677ad9f0f4c6277a
Lennse Garcia <lennseg@Lennses-MacBook-Pro.local>
2017-04-25 18:15:25 -0400

    building the dataset

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
+/* buttons */
+
 $("#women-button").click(function () {
     $(".women").fadeIn(100);
     $(".men").fadeOut(1000);
 });
-
 $("#men-button").click(function () {
-    //$("#men-button").css("background", "#b5b8bd");
     $(".men").fadeIn(100);
     $(".women").fadeOut(1000);
 });
 $("#nominees-button").click(function () {
     $(".nominee").fadeIn(10);
 });
+
+/* objects */
+
+var actors;
+
+kennedy = document.getElementById("kennedy")
+kennedy.addEventListener("click",function(){
+  console.log(actors["kennedy"].nominations)
+})
