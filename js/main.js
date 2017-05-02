c3f956d4c0be1ee7197d4b50dbb0f48cb366a70d
Lennse Garcia <lennseg@Lennses-MBP.attlocal.net>
2017-05-02 12:21:39 -0400

    debugging

--- js/main.js
+++ js/main.js
@@ -19,26 +19,3 @@ khepburn = document.getElementById("khepburn")
 khepburn.addEventListener("click",function(){
   console.log(actors["khepburn"].films)
 })
-
-/* parse data */
-$(document).ready(function() {
-    $.ajax ({
-        type: 'GET',
-        url: 'data/data.json',
-        dataType:'json',
-        success: jsonParser
-    });
-});
-    
-function jsonParser(json) {
-    $('#load').fadeOut();
-     $.getJSON('data/data.json',function(json){
-         $.each(data.actors.streep,function(k,v){
-             var name = v.name;
-             var nominations = v.nominations;
-             var wins = v.wins;
-             var genres = v.genres;
-             $('#container').append('div class="streep"> + nominations + </div>')
-         });
-    });
-}
