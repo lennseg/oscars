624e7e7e652733298362c841f78527bdc4ec63ff
Lennse Garcia <lennseg@Lennses-MBP.attlocal.net>
2017-05-03 12:24:07 -0400

    loading data js

--- js/main.js
+++ js/main.js
@@ -10,25 +10,41 @@ $("#men-button").click(function () {
 $("#nominees-button").click(function () {
     $(".nominee").fadeIn(10);
 });
-
 /* loading data */
 
 var list = document.querySelector("ul");
 
 list.addEventListener("click", function (event) {
-    console.dir(event.target.parentElement.id);
-    document.querySelector("ms").textContent = "Test" + actors[event.target.id].wins;
+    console.log(event.target.parentElement.id);
+    document.querySelector("#name").textContent = actors[event.target.parentElement.id].name
+    document.querySelector("#nominations").textContent = actors[event.target.parentElement.id].nominations
+    document.querySelector("#wins").textContent = actors[event.target.parentElement.id].wins
+    document.querySelector("#genres").textContent = actors[event.target.parentElement.id].genres
 });
 
-var actors; /* div for data */
+/* div for data */
 
+var actors; 
 //opens actor window
 streep = document.getElementById("streep")
 streep.addEventListener("click", function () {
     console.log(actors["streep"].wins);
-    document.getElementById("ms").style.display = "block";
+    document.getElementById("modal").style.display = "block";
 });
+
+khepburn = document.getElementById("khepburn")
+khepburn.addEventListener("click", function () {
+    console.log(actors["khepburn"].wins);
+    document.getElementById("modal").style.display = "block";
+});
+
+nicholson = document.getElementById("nicholson")
+nicholson.addEventListener("click", function () {
+    console.log(actors["nicholson"].wins);
+    document.getElementById("modal").style.display = "block";
+});
+
 //closes actor window
 $("#closebutton").on("click", function () {
-    $("#ms").css('display', 'none');
+    $("#modal").css('display', 'none');
 });
